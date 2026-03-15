a01c4f64b6883202212d013d40aad36f85674352
Rohan Nair <rohanmanunair@gmail.com>
2026-03-15 17:42:04 -0400

    Error findig should be done (minus tests)

--- src/include/OdometryCorrection.h
+++ src/include/OdometryCorrection.h
@@ -19,23 +19,40 @@ struct OdometeryCorrection {
         return std::hypot(errorX, errorY);
     }
 
-    WorldCoord deriveCoordFromIR() {
-        double sumX = 0;
-        double sumY = 0;
+    WorldCoord deriveCoordFromIR()
+    {
+        if (readings.empty())
+            return cur;
 
+        double wx = 0, wy = 0, totalWeight = 0;
+        auto ref = *std::min_element(readings.begin(), readings.end(),
+                                     [](WorldCoord &a, WorldCoord &b)
+                                     {
+                                         return a.hypot() < b.hypot();
+                                     });
 
-        for (int i = 0; i < readings.size(); i++) {
-
-            sumX += readings.at(i).x;
-            sumY += readings.at(i).y;
-   
-
+        for (size_t i = 0; i < readings.size(); i++)
+        {
+            if (sameObject(readings[i], ref, 0.1))
+            {
+                double w = 1.0 / (readings[i].hypot() + 0.01);
+                wx += readings[i].x * w;
+                wy += readings[i].y * w;
+                totalWeight += w;
+            }
         }
-        sumX /= readings.size();
-        sumY /= readings.size();
-        return {sumX, sumY};
-        // logic to derive world coord from IR readings
-        return {};
+
+        if (totalWeight == 0)
+            return cur;
+        wx /= totalWeight;
+        wy /= totalWeight;
+        return {wx, wy, cur.theta};
+    }
+
+    bool sameObject(WorldCoord readingA, WorldCoord readingB, double threshold)
+    {
+        double dist = std::hypot(readingA.x - readingB.x, readingA.y - readingB.y);
+        return dist < threshold;
     }
 };
 
