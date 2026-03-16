586d0f7907ba95f6891c0aabc25cbdf3db16ad5d
jdugan35 <jdugan35@gatech.edu>
2026-03-16 18:44:27 -0400

    formatting

--- src/include/OdometryCorrection.h
+++ src/include/OdometryCorrection.h
@@ -1,68 +1,63 @@
+#include "Constants.h"
 #include "Helpers.h"
-#include "Types.h"
-#include <cmath>
-#include <cstdint>
 #include "IRSensor.h"
 #include "Pins.h"
-#include "Constants.h"
+#include "Types.h"
 #include <Arduino.h>
 #include <DRV8833.h>
+#include <cmath>
+#include <cstdint>
+
 
 struct OdometeryCorrection {
-    WorldCoord cur{};
-    std::vector<WorldCoord> readings{};
+  WorldCoord cur{};
+  std::vector<WorldCoord> readings{};
 
-    double getError() {
-        WorldCoord derived = deriveCoordFromIR();
-        double errorX = derived.x - cur.x;
-        double errorY = derived.y - cur.y;
-        return std::hypot(errorX, errorY);
+  double getError() {
+    WorldCoord derived = deriveCoordFromIR();
+    double errorX = derived.x - cur.x;
+    double errorY = derived.y - cur.y;
+    return std::hypot(errorX, errorY);
+  }
+
+  WorldCoord deriveCoordFromIR() {
+    if (readings.empty())
+      return cur;
+
+    double wx = 0, wy = 0, totalWeight = 0;
+    // Find the closest reading as a reference point
+    auto ref = *std::min_element(
+        readings.begin(), readings.end(),
+        [](WorldCoord &a, WorldCoord &b) { return a.hypot() < b.hypot(); });
+
+    for (size_t i = 0; i < readings.size(); i++) {
+      if (sameObject(readings[i], ref, 0.1)) {
+        double w = 1.0 / (readings[i].hypot() + 0.01);
+        wx += readings[i].x * w;
+        wy += readings[i].y * w;
+        totalWeight += w;
+      }
     }
 
-    WorldCoord deriveCoordFromIR()
-    {
-        if (readings.empty())
-            return cur;
+    if (totalWeight == 0)
+      return cur;
+    wx /= totalWeight;
+    wy /= totalWeight;
+    return {wx, wy, cur.theta};
+  }
 
-        double wx = 0, wy = 0, totalWeight = 0;
-        // Find the closest reading as a reference point
-        auto ref = *std::min_element(readings.begin(), readings.end(),
-                                     [](WorldCoord &a, WorldCoord &b)
-                                     {
-                                         return a.hypot() < b.hypot();
-                                     });
+  bool sameObject(WorldCoord readingA, WorldCoord readingB, double threshold) {
+    double dist = std::hypot(readingA.x - readingB.x, readingA.y - readingB.y);
+    return dist < threshold;
+  }
 
-        for (size_t i = 0; i < readings.size(); i++)
-        {
-            if (sameObject(readings[i], ref, 0.1))
-            {
-                double w = 1.0 / (readings[i].hypot() + 0.01);
-                wx += readings[i].x * w;
-                wy += readings[i].y * w;
-                totalWeight += w;
-            }
-        }
-
-        if (totalWeight == 0)
-            return cur;
-        wx /= totalWeight;
-        wy /= totalWeight;
-        return {wx, wy, cur.theta};
-    }
-
-    bool sameObject(WorldCoord readingA, WorldCoord readingB, double threshold)
-    {
-        double dist = std::hypot(readingA.x - readingB.x, readingA.y - readingB.y);
-        return dist < threshold;
-    }
-
-    WorldCoord correct() {
-        double error = getError();
-        double trust = IR_SENSOR_TRUST;
-        double correctedX = (1-trust) * cur.x + trust * (deriveCoordFromIR().x - cur.x);
-        double correctedY = (1-trust) * cur.y + trust * (deriveCoordFromIR().y - cur.y);
-        return {correctedX, correctedY, cur.theta};
-
-        }
+  WorldCoord correct() {
+    double error = getError();
+    double trust = IR_SENSOR_TRUST;
+    double correctedX =
+        (1 - trust) * cur.x + trust * (deriveCoordFromIR().x - cur.x);
+    double correctedY =
+        (1 - trust) * cur.y + trust * (deriveCoordFromIR().y - cur.y);
+    return {correctedX, correctedY, cur.theta};
+  }
 };
-
