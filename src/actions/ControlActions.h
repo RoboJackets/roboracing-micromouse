bb194f83c02a319f27ba9424717bc2a8f928790e
jdugan35 <jdugan35@gatech.edu>
2026-03-23 13:10:48 -0400

    twea

--- src/actions/ControlActions.h
+++ src/actions/ControlActions.h
@@ -184,7 +184,7 @@ struct ProfiledDriveAction : Action {
     if (std::abs(io.getSensorState().at(2).x) < 0.16 &&
         io.getSensorState().at(3).x < 0.16) {
       c = irPID.calculate(io.getSensorState().at(3).x,
-                          -io.getSensorState().at(2).x - 0.01, io.getDt());
+                          -io.getSensorState().at(2).x - 0.03, io.getDt());
     } else if (std::abs(io.getSensorState().at(2).x) < 0.16) {
       c = irPID.calculate(io.getSensorState().at(2).x, -0.085, io.getDt());
     } else if (io.getSensorState().at(3).x < 0.16) {
